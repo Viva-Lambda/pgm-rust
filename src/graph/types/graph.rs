acaaeb439ebad328bb9a56feb3160b5a5fe5304b
Viva Lambda <76657254+Viva-Lambda@users.noreply.github.com>
2022-10-25 23:22:09 +0900

    closes #4

--- src/graph/types/graph.rs
+++ src/graph/types/graph.rs
@@ -134,3 +134,123 @@ impl Graph {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*; // brings in the parent scope to current module scope
+
+    // mk node
+    fn mk_node(n_id: &str) -> Node {
+        Node::new(n_id.to_string(), HashMap::new())
+    }
+
+    // mk edge
+    fn mk_uedge(n1_id: &str, n2_id: &str, e_id: &str) -> Edge {
+        let n1 = mk_node(n1_id);
+        let n2 = mk_node(n2_id);
+        let mut h1 = HashMap::new();
+        h1.insert(String::from("my"), vec![String::from("data")]);
+        Edge::undirected(e_id.to_string(), n1, n2, h1)
+    }
+
+    // make graph
+    fn mk_g(g_id: &str) -> Graph {
+        let mut nodes = HashSet::new();
+        let mut edges = HashSet::new();
+        nodes.insert(mk_node("n1"));
+        nodes.insert(mk_node("n2"));
+        nodes.insert(mk_node("n3"));
+        nodes.insert(mk_node("n4"));
+        edges.insert(mk_uedge("n1", "n2", "e1"));
+        edges.insert(mk_uedge("n2", "n3", "e2"));
+        Graph::new(g_id.to_string(), nodes, edges, HashMap::new())
+    }
+
+    //
+    #[test]
+    fn test_vertices() {
+        let g = mk_g("g1");
+        let vs = g.vertices();
+        //
+        let mut nodes = HashSet::new();
+        nodes.insert(mk_node("n1"));
+        nodes.insert(mk_node("n2"));
+        nodes.insert(mk_node("n3"));
+        nodes.insert(mk_node("n4"));
+        //
+        assert_eq!(&nodes, vs);
+    }
+
+    #[test]
+    fn test_edges() {
+        let g = mk_g("g1");
+        let es = g.edges();
+        //
+        let mut edges = HashSet::new();
+        edges.insert(mk_uedge("n1", "n2", "e1"));
+        edges.insert(mk_uedge("n2", "n3", "e2"));
+        //
+        assert_eq!(&edges, es);
+    }
+    #[test]
+    fn test_from_graphish_ref() {
+        let g1 = mk_g("g1");
+        let g2 = mk_g("g1");
+        let g = Graph::from_graphish_ref(&g1);
+        assert_eq!(g, g2);
+    }
+
+    #[test]
+    fn test_from_graphish() {
+        let g1 = mk_g("g1");
+        let g2 = mk_g("g1");
+        let g = Graph::from_graphish(g1);
+        assert_eq!(g, g2);
+    }
+
+    #[test]
+    fn test_from_edgeset() {
+        let mut edges = HashSet::new();
+        edges.insert(mk_uedge("n1", "n2", "e1"));
+        edges.insert(mk_uedge("n2", "n3", "e2"));
+        let g1 = Graph::from_edgeset(edges.clone());
+
+        let mut nodes = HashSet::new();
+        nodes.insert(mk_node("n1"));
+        nodes.insert(mk_node("n2"));
+        nodes.insert(mk_node("n3"));
+        assert_eq!(g1.vertices(), &nodes);
+        assert_eq!(g1.edges(), &edges);
+    }
+
+    #[test]
+    fn test_from_edge_node_set() {
+        let mut nodes = HashSet::new();
+        let mut edges = HashSet::new();
+        nodes.insert(mk_node("n1"));
+        nodes.insert(mk_node("n2"));
+        nodes.insert(mk_node("n3"));
+        nodes.insert(mk_node("n4"));
+        edges.insert(mk_uedge("n1", "n2", "e1"));
+        edges.insert(mk_uedge("n2", "n3", "e2"));
+        let g = Graph::from_edge_node_set(edges.clone(), nodes.clone());
+        assert_eq!(g.vertices(), &nodes);
+        assert_eq!(g.edges(), &edges);
+    }
+
+    #[test]
+    fn test_based_on_node_set() {
+        let mut nodes = HashSet::new();
+        let mut edges = HashSet::new();
+        nodes.insert(mk_node("n2"));
+        nodes.insert(mk_node("n3"));
+        edges.insert(mk_uedge("n1", "n2", "e1"));
+        edges.insert(mk_uedge("n2", "n3", "e2"));
+        let g = Graph::based_on_node_set(edges.clone(), nodes.clone());
+        assert_eq!(g.vertices(), &nodes);
+        let mut es = HashSet::new();
+        es.insert(mk_uedge("n2", "n3", "e2"));
+        assert_eq!(g.edges(), &es);
+    }
+}
