f206c4df5d5ba3e75bae68a8c9c1d34afefbd1dd
Viva Lambda <76657254+Viva-Lambda@users.noreply.github.com>
2022-10-25 23:44:18 +0900

    closes #6

--- src/graph/ops/edge/nodeops.rs
+++ src/graph/ops/edge/nodeops.rs
@@ -12,11 +12,33 @@ pub fn get_other<'a>(e: &'a Edge, n: &Node) -> &'a Node {
     let eid = end.id();
     if sid == nid {
         e.end()
-    } else if sid == eid {
+    } else if eid == nid {
         e.start()
     } else {
         let this_edge = dbg!(e);
         let this_node = dbg!(n);
         panic!("{n} does not belong to this {this_edge}");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::collections::HashMap; // brings in the parent scope to current module scope
+
+    fn mk_uedge() -> Edge {
+        let n1 = Node::new(String::from("m1"), HashMap::new());
+        let n2 = Node::new(String::from("m2"), HashMap::new());
+        let mut h1 = HashMap::new();
+        h1.insert(String::from("my"), vec![String::from("data")]);
+        Edge::undirected(String::from("uedge"), n1, n2, h1)
+    }
+    #[test]
+    fn test_get_other() {
+        let e = mk_uedge();
+        let n2 = Node::new(String::from("m2"), HashMap::new());
+        let n1 = Node::new(String::from("m1"), HashMap::new());
+        assert_eq!(get_other(&e, &n2), &n1);
     }
 }
