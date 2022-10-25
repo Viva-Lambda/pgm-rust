2be20dfc8d7439dc3bde6b0109f24a378fb563fd
Viva Lambda <76657254+Viva-Lambda@users.noreply.github.com>
2022-10-25 23:38:33 +0900

    closes #5

--- src/graph/ops/edge/boolops.rs
+++ src/graph/ops/edge/boolops.rs
@@ -11,12 +11,12 @@ pub fn is_start(e: &Edge, n: &Node) -> bool {
 pub fn is_end(e: &Edge, n: &Node) -> bool {
     e.end().id() == n.id()
 }
-
 pub fn is_endvertice(e: &Edge, n: &Node) -> bool {
     let ids = node_ids(&e);
     let nid: &String = &n.id();
     ids.contains(nid)
 }
+
 #[cfg(test)]
 mod tests {
 
@@ -41,5 +41,17 @@ mod tests {
         let e = mk_uedge();
         let n1 = Node::new(String::from("m3"), HashMap::new());
         assert!(!is_endvertice(&e, &n1)); //
+    }
+    #[test]
+    fn test_is_start() {
+        let e = mk_uedge();
+        let n1 = Node::new(String::from("m1"), HashMap::new());
+        assert!(is_start(&e, &n1));
+    }
+    #[test]
+    fn test_is_end() {
+        let e = mk_uedge();
+        let n2 = Node::new(String::from("m2"), HashMap::new());
+        assert!(is_end(&e, &n2));
     }
 }
