3eeaa548aeb3dbb523cda9cb04fd56d190d1f23e
Viva-Lambda <76657254+Viva-Lambda@users.noreply.github.com>
2024-08-16 02:49:19 +0900

    added tree trait

--- src/graph/traits.rs
+++ src/graph/traits.rs
@@ -3,13 +3,13 @@
 /// behaviors that defines a [Graph]
 pub mod graph;
 
-/// behaviors that defines a [GraphObject] 
+/// behaviors that defines a [GraphObject]
 /// everything that implements a [GraphObject] can be used as a node.
 pub mod graph_obj;
 
 /// behaviors that defines a [Node]
 /// Essentially it is a very thin wrapper over [GraphObject]. They only look
-/// different when represented as strings. 
+/// different when represented as strings.
 pub mod node;
 
 /// behaviors that defines an [Edge]
@@ -17,3 +17,6 @@ pub mod edge;
 
 /// diverse behaviors that help with lib implementation
 pub mod misc;
+
+/// behaviors that defines a [Tree]
+pub mod tree;
---
+++ src/graph/traits/tree.rs
@@ -0,0 +1,43 @@
+// tree trait
+use crate::graph::traits::edge::Edge;
+use crate::graph::traits::graph::Graph;
+use crate::graph::traits::graph_obj::GraphObject;
+use crate::graph::traits::node::Node;
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+/// a tree
+pub trait Tree<N: Node, E: Edge<N>>: Graph<N, E> {
+    /// From Diestel 2017, p. 15
+    /// is x up closure of y
+    fn is_upclosure_of(&self, x_src: &N, y_dst: &N) -> bool;
+
+    /// From Diestel 2017, p. 15
+    /// is x down closure of y
+    fn is_downclosure_of(&self, x_src: &N, y_dst: &N) -> bool;
+
+    /// From Diestel 2017, p. 15
+    fn upset_of(&self, x_src: &N) -> HashSet<&N>;
+
+    /// From Diestel 2017, p. 15
+    fn downset_of(&self, x_src: &N) -> HashSet<&N>;
+
+    /// root of tree
+    fn root(&self) -> &N;
+
+    /// leaves of tree
+    fn leaves(&self) -> HashSet<&N>;
+
+    /// height of node
+    fn height_of(&self, n: &N) -> i32;
+
+    // nodes per level in python
+    /// extract nodes based on height
+    fn nodes_per_height(&self, height: i32) -> HashSet<&N>;
+
+    /// <= comparison for nodes of tree
+    fn less_than_or_equal(&self, first: &N, second: &N) -> bool;
+
+    /// >= comparison for nodes of tree
+    fn greater_than_or_equal(&self, first: &N, second: &N) -> bool;
+}
